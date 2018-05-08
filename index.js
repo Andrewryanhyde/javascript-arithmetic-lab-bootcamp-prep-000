48a96b98114a54d8904ee08cbe69a67fd488e121
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-08 12:45:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-function multiplyBy5 (number) {
-  return number * 5;
+function add (a, b) {
+  return a + b;
 }
+
+function subtract (a, b) {
+  return a-b;
+}
+
+function multiply (a, b) {
+  return a * b; 
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  return n++;
+}
+
+function dec(n) {
+  return n--; 
+}
+
+function makeInt(n) {
+  parseInt(n)
+  return n;
+}
+
+
+
+function preserveDecimal(n, 10) {
+  makeFloat(n) 
+  return n;
+}
+
+
+
+
+
+
+
 
